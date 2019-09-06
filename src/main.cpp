b2e0cf7d19d44c03c46df0ad043f3cbe6e05f38d
Iwan Boksebeld <iwan.boksebeld@gmail.com>
2019-09-06 15:25:53 +0200

    PreProcessing most information

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/opengl/glfw/imgui/ImGuiHelpers.h>
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
-#include <igl/readOFF.h>
+#include <igl/read_triangle_mesh.h>
 #include <imgui/imgui.h>
+
+#include <algorithm>
+#include <experimental/filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #ifndef ASSET_PATH
 #define ASSET_PATH "data"
 #endif
 
-Eigen::MatrixXd V;
-Eigen::MatrixXi F;
+using namespace Eigen;
+using namespace std;
+namespace filesys = std::experimental::filesystem;
+
+MatrixXd V;
+MatrixXi F;
 
 class CustomMenu : public igl::opengl::glfw::imgui::ImGuiMenu
 {
@@ -35,8 +47,115 @@ bool key_down(igl::opengl::glfw::Viewer &viewer, unsigned char key, int modifier
 	return false;
 }
 
+static bool endsWith(const std::string &str, const std::string &ending)
+{
+	if (str.length() < ending.length())
+		return false;
+	return str.compare(str.length() - ending.length(), ending.length(), ending) == 0;
+}
+
+// Based on
+// https://thispointer.com/c-get-the-list-of-all-files-in-a-given-directory-and-its-sub-directories-using-boost-c17/
+/*
+ * Get the list of all files in given directory and its sub directories.
+ *
+ * Arguments
+ * 	dirPath : Path of directory to be traversed
+ * 	dirSkipList : List of folder names to be skipped
+ *
+ * Returns:
+ * 	vector containing paths of all the files in given directory and its sub directories
+ *
+ */
+vector<string> getAllFilesInDir(const string &dirPath, const vector<string> dirSkipList = {})
+{
+
+	// Create a vector of string
+	std::vector<string> listOfFiles;
+	try
+	{
+		// Check if given path exists and points to a directory
+		if (filesys::exists(dirPath) && filesys::is_directory(dirPath))
+		{
+			// Create a Recursive Directory Iterator object and points to the starting of directory
+			filesys::recursive_directory_iterator iter(dirPath);
+
+			// Create a Recursive Directory Iterator object pointing to end.
+			filesys::recursive_directory_iterator end;
+
+			// Iterate till end
+			while (iter != end)
+			{
+				// Check if current entry is a directory and if exists in skip list
+				if (filesys::is_directory(iter->path()) &&
+					(std::find(dirSkipList.begin(), dirSkipList.end(), iter->path().filename()) !=
+					 dirSkipList.end()))
+					// Skip the iteration of current directory pointed by iterator
+					iter.disable_recursion_pending();
+				else if (!filesys::is_directory(iter->path()))
+					// Add the name in vector
+					listOfFiles.push_back(iter->path().string());
+
+				error_code ec;
+				// Increment the iterator to point to next entry in recursive iteration
+				iter.increment(ec);
+				if (ec)
+				{
+					cerr << "Error While Accessing : " << iter->path().string()
+						 << " :: " << ec.message() << endl;
+				}
+			}
+		}
+	}
+	catch (system_error &e)
+	{
+		cerr << "Exception :: " << e.what();
+	}
+	return listOfFiles;
+}
+
+/*
+string split implementation by using delimiter as a character.
+*/
+vector<string> split(string strToSplit, char delimeter)
+{
+	stringstream ss(strToSplit);
+	string item;
+	vector<string> splittedStrings;
+	while (getline(ss, item, delimeter))
+	{
+		splittedStrings.push_back(item);
+	}
+	return splittedStrings;
+}
+
+void preProcessMeshDatabase(const vector<string> &files)
+{
+	for (auto &f : files)
+		if (endsWith(f, ".off") || endsWith(f, ".ply"))
+		{
+			auto splitted = split(f, '\\');
+			igl::read_triangle_mesh(f, V, F);
+
+			ofstream infoFile;
+			infoFile.open(f + ".info");
+			infoFile << "mesh\t\t" << f << endl;
+			infoFile << "class\t\t" << splitted[max((int)splitted.size() - 2, 0)] << endl;
+			infoFile << "vertices\t" << V.rows() << endl;
+			infoFile << "faces\t\t" << F.rows() << endl;
+			infoFile << "facetype\t"
+					 << "triangle" << endl; // TODO
+			infoFile << "mincorner\t" << V.colwise().minCoeff() << endl;
+			infoFile << "maxcorner\t" << V.colwise().maxCoeff() << endl;
+			infoFile.close();
+		}
+}
+
 int main(int argc, char *argv[])
 {
+	vector<string> files = getAllFilesInDir(ASSET_PATH "LabeledDB_new/");
+	preProcessMeshDatabase(files);
+
 	// Load a mesh in OFF format
 	igl::readOFF(ASSET_PATH "/bunny.off", V, F);
 
