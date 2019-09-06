808cae9993ab6c3f51147dba0b3d6ee9cf406bee
Iwan Boksebeld <iwan.boksebeld@gmail.com>
2019-09-06 13:57:04 +0200

    Multiple viewport sample code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #define ASSET_PATH "data"
 #endif
 
+// Plot the mesh
+igl::opengl::glfw::Viewer viewer;
+
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 
@@ -13,12 +16,52 @@ int main(int argc, char *argv[])
 	// Load a mesh in OFF format
 	igl::readOFF(ASSET_PATH "/bunny.off", V, F);
 
-	// Plot the mesh
 	igl::opengl::glfw::Viewer viewer;
+
 	viewer.data().set_mesh(V, F);
 	viewer.launch();
+	return EXIT_SUCCESS;
 }
 
+// Sample code for multiple viewports
+#if 0
+igl::opengl::glfw::Viewer viewer;
+
+viewer.load_mesh_from_file(ASSET_PATH "/bunny.off");
+viewer.load_mesh_from_file(ASSET_PATH "/bunny.off");
+
+unsigned int left_view, right_view;
+int cube_id = viewer.data_list[0].id;
+int sphere_id = viewer.data_list[1].id;
+viewer.callback_init = [&](igl::opengl::glfw::Viewer &) {
+	viewer.core().viewport = Eigen::Vector4f(0, 0, 640, 800);
+	left_view = viewer.core_list[0].id;
+	right_view = viewer.append_core(Eigen::Vector4f(640, 0, 640, 800));
+	return false;
+};
+
+viewer.callback_key_down = [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod) {
+	if (key == GLFW_KEY_SPACE)
+	{
+		// By default, when a core is appended, all loaded meshes will be displayed in that
+		// core. Displaying can be controlled by calling viewer.data().set_visible().
+		viewer.data(cube_id).set_visible(false, left_view);
+		viewer.data(sphere_id).set_visible(false, right_view);
+	}
+	return false;
+};
+
+viewer.callback_post_resize = [&](igl::opengl::glfw::Viewer &v, int w, int h) {
+	v.core(left_view).viewport = Eigen::Vector4f(0, 0, w / 2, h);
+	v.core(right_view).viewport = Eigen::Vector4f(w / 2, 0, w - (w / 2), h);
+	return true;
+};
+
+viewer.launch();
+
+#endif
+
+// Code from Game Physics practical
 #if 0
 #include "scene.h"
 #include <igl/opengl/glfw/Viewer.h>
